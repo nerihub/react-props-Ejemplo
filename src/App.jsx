1a69d683cf7931a4b5cc89615eb689701138b9bb
nerihub <neri_150487@hotmail.com>
2021-03-26 12:40:14 -0600

    1 file modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-
+import Cards from './compounent/Cards'
 
 function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <h1>Propiedades de los Componentes</h1>
+
+      <div className="row">
+        <div className="col">
+          <Cards
+            imagen = "https://lorempixel.com/150/150"
+            titulo = "Titulo Card 1"
+            texto = "Texto Card 1"
+          />         
+        </div>
+        <div className="col">
+          <Cards
+            imagen = "https://lorempixel.com/150/150"
+            titulo = "Titulo Card 2"
+            texto = "Texto Card 2"
+          />         
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/compounent/Cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Cards = (props) =>{
+
+  const {imagen,titulo,texto} = props;
+
+  return (
+    <div className="card" sytle={{width: "18rem"}}>
+      <img src={imagen} className="card-img-top img-thumbnail" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{titulo}</h5>
+        <p className="card-text">{texto}</p>
+        <a hreg="#" className="btn btn-primary">Ver Mas..</a>
+      </div>
+    </div>
+  );
+
+}
+export default Cards;
